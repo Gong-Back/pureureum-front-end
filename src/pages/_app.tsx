b7a27275f515e86fd76ec8b241071453027955e8
RookieAND <gwangin1999@naver.com>
2023-04-02 19:02:33 +0900

    PRR-38 [feat]: GlobalStyle 및 theme Provider를 _app 에 주입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { Provider } from 'jotai';
+
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { ThemeProvider, Global } from '@emotion/react';
+import { theme, GlobalStyle } from '@/constants/styles';
 
 import '@/assets/fonts/font.css';
 
@@ -22,8 +26,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <Provider>
-        {/* eslint-disable react/jsx-props-no-spreading */}
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Global styles={GlobalStyle} />
+          {/* eslint-disable react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     </QueryClientProvider>
   );
