54a204f54e226e514c0d81ac1691253a4fc469e0
gwangin1999@naver.com <gwangin1999@naver.com>
2023-04-09 21:59:09 +0900

    PRR-45 [feat]: 마이페이지 사이드 바 UI 구축

---
+++ src/components/domain/MyPage/SideNavigationBar/SideNavigationBar.style.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.aside(({ theme }) => {
+  const { colors } = theme;
+  return {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '69px 0px',
+
+    width: '220px',
+    height: '100vh',
+
+    padding: '68px 50px 0px 50px',
+
+    background: colors.grayscale.background,
+    borderRight: `1px solid ${colors.grayscale.gray100}`,
+  };
+});
+
+export const NavGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px 0px;
+`;
+
+export const NavGroupTitle = styled.h5(({ theme }) => {
+  const { colors, fonts } = theme;
+  return {
+    margin: '0px',
+
+    color: colors.grayscale.gray700,
+    ...fonts.pc.body2B,
+    textAlign: 'right',
+  };
+});
+
+export const NavItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 15px 0px;
+
+  margin: auto 0px;
+`;
+
+export const NavItemGroup = styled.div`
+  display: flex;
+  gap: 0px 6px;
+
+  & > svg {
+    margin: auto 0px;
+  }
+`;
+
+export const NavItem = styled.p(({ theme }) => {
+  const { colors, fonts } = theme;
+  return {
+    margin: '0px',
+    color: colors.grayscale.gray500,
+    ...fonts.pc.body3,
+  };
+});
