d6f7976b0ee0a62a3018c1f31d022865d51a77b8
gwangin1999@naver.com <gwangin1999@naver.com>
2023-04-09 21:59:33 +0900

    PRR-45 [feat]: 마이페이지 내 프로필 페이지 템플릿 임시 구축

---
+++ src/components/template/MyProfileTemplate/MyProfileTemplate.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SideNavigationBar from '@/components/domain/MyPage/SideNavigationBar';
+
+import * as style from './MyProfileTemplate.style';
+
+interface MyProfileTemplatesProps {}
+
+const MyProfileTemplate = () => (
+  <style.Wrapper>
+    <SideNavigationBar />
+  </style.Wrapper>
+);
+
+export default MyProfileTemplate;
