c2fe8f60c40ddb2f59da58cdeee6b0206a45283b
ballsona <ballsona0925@naver.com>
2023-04-16 18:11:53 +0900

    PRR-47 [feat]: 컴포넌트 props 주석 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import * as styles from './Button.style';
 
 export interface ButtonProps {
+  children: React.ReactNode;
+  /** 높이를 기준으로 분류되는 사이즈 타입 (각각 높이: 35/40/45px) */
   sizeType?: 'small' | 'medium' | 'large';
+  /** 버튼 컴포넌트 너비 */
   width?: number;
+  /** 버튼 텍스트 색상 (default: colors.grayscale.white) */
   textColor?: string;
+  /** 버튼 테두리 색상 (default: none) */
   borderColor?: string;
+  /** 버튼 배경 색상 (default: colors.primary.greendefault) */
   backgroundColor?: string;
+  /** 버튼 테두리 둥글게 할지 여부 */
   isRound?: boolean;
   className?: string;
-  children: React.ReactNode;
   onClick: () => void;
 }
 
--- src/components/common/TextInput/TextInput.tsx
+++ src/components/common/TextInput/TextInput.tsx
@@ -1,16 +1,23 @@
 import React, { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 import * as styles from './TextInput.style';
 
+// TODO Input 배경색 색상 변수에 등록
 export interface TextInputProps
   extends DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   > {
+  /** Input value */
   value: string;
+  /** Input placeholder */
   placeholder: string;
+  /** 높이를 기준으로 분류되는 사이즈 타입 (각각 높이: 45/50/60px) */
   sizeType?: 'small' | 'medium' | 'large';
+  /** Input 컴포넌트 너비 */
   width?: number;
+  /** 배경색 채울지에 대한 여부 (배경색 : #F8F8F8) */
   isFilled?: boolean;
+  /** Input 테두리 둥글게 할지 여부  */
   isRound?: boolean;
   className?: string;
 }
@@ -26,6 +33,7 @@ const TextInput = ({
   ...inputprops
 }: TextInputProps) => (
   <styles.Input
+    type="text"
     sizeType={sizeType}
     isFilled={isFilled}
     isRound={isRound}
