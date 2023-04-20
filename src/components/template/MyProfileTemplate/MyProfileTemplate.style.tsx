80ba90b705dd824d6b6f3c0bb015dbe3bf1df031
RookieAND <gwangin1999@naver.com>
2023-04-20 22:48:42 +0900

    PRR-45 [feat]: 프로필 관련 섹션 UI 일부 추가

--- src/components/template/MyProfileTemplate/MyProfileTemplate.style.tsx
+++ src/components/template/MyProfileTemplate/MyProfileTemplate.style.tsx
@@ -2,12 +2,7 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.section`
   display: flex;
-  flex-direction: column;
   justify-content: space-between;
-  gap: 55px 0px;
-
-  width: 400px;
-  margin: 0px;
 `;
 
 export const Header = styled.header`
@@ -16,65 +11,79 @@ export const Header = styled.header`
   gap: 10px 0px;
 `;
 
-export const VisibleSection = styled.div`
-  width: 100%;
-  overflow: hidden;
-`;
-
-export const Section = styled.section<{ currentRegisterStep: number }>(
-  ({ currentRegisterStep }) => ({
-    display: 'flex',
-    gap: '16px 0px',
-
-    transform: `translateX(${-100 * currentRegisterStep}%)`,
-    transition: '0.25s all ease-in-out',
-  }),
-);
-
-export const Footer = styled.footer`
+export const Main = styled.section`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  gap: 10px 0px;
+
+  width: 600px;
+  margin: 94px auto;
 `;
 
-export const Title = styled.h5(({ theme }) => {
-  const { colors, fonts } = theme;
+export const Section = styled.section(({ theme }) => {
+  const { colors } = theme;
   return {
-    margin: '0px',
+    display: 'flex',
+    gap: '0px 25px',
 
-    color: colors.grayscale.dark,
-    ...fonts.pc.subtitle1,
+    width: '600px',
+
+    padding: '15px 30px 15px 40px',
+
+    border: `1px solid ${colors.grayscale.gray100}`,
+    borderRadius: '10px',
   };
 });
 
-export const Subtitle = styled.p(({ theme }) => {
+export const Label = styled.p(({ theme }) => {
   const { colors, fonts } = theme;
   return {
-    margin: '0px',
+    width: '100px',
+    margin: '4px auto 4px 0px',
 
-    color: colors.grayscale.gray500,
-    ...fonts.pc.body1R,
+    ...fonts.pc.body2B,
+    color: `${colors.grayscale.gray700}`,
   };
 });
 
-export const Feedback = styled.p(({ theme }) => {
+export const Content = styled.span(({ theme }) => {
   const { colors, fonts } = theme;
   return {
-    margin: '0px',
+    flexGrow: '1',
+    margin: '4px 0px',
 
-    color: colors.primary.caption,
-    ...fonts.pc.caption,
+    ...fonts.pc.body2R,
+    color: `${colors.grayscale.gray700}`,
   };
 });
+
+export const ChangeButton = styled.button<{ width?: number }>(
+  ({ theme, width = '100px' }) => {
+    const { colors, fonts } = theme;
+    return {
+      width,
+      height: '35px',
+
+      marginLeft: 'auto',
+      padding: '6px auto',
+
+      backgroundColor: colors.grayscale.white,
+      border: `1px solid ${colors.primary.greenDefault}`,
+      borderRadius: '25px',
+
+      color: colors.primary.greenDefault,
+      ...fonts.pc.body3,
+    };
+  },
+);
 
 export const ConfirmButton = styled.button<{ isConfirm: boolean }>(
   ({ theme, isConfirm = false }) => {
     const { colors, fonts } = theme;
     return {
-      width: '100%',
-      height: '46px',
+      width: '300px',
+      height: '45px',
 
+      marginRight: 'auto',
       padding: '9px auto',
 
       backgroundColor: isConfirm
--- src/components/template/MyProfileTemplate/MyProfileTemplate.tsx
+++ src/components/template/MyProfileTemplate/MyProfileTemplate.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 
+import { GenderType } from '@/constants/types';
+
+import BasicInformation from '@/components/domain/MyPage/Profile/BasicInformation';
 import SideNavigationBar from '@/components/domain/MyPage/SideNavigationBar';
 
 import * as style from './MyProfileTemplate.style';
 
-interface MyProfileTemplatesProps {}
+interface MyProfileTemplatesProps {
+  name: string;
+  email: string;
+  gender: GenderType;
+  birthday: string;
+  phoneNumber: string;
+}
 
-const MyProfileTemplate = () => (
+const MyProfileTemplate = ({
+  name,
+  email,
+  gender,
+  birthday,
+  phoneNumber = '010-7167-0851',
+}: MyProfileTemplatesProps) => (
   <style.Wrapper>
     <SideNavigationBar />
+    <style.Main>
+      <BasicInformation
+        name={name}
+        email={email}
+        gender={gender}
+        birthday={birthday}
+      />
+      <style.Section>
+        <style.Label>휴대폰 번호</style.Label>
+        <style.Content>{phoneNumber}</style.Content>
+        <style.ChangeButton>번호 변경</style.ChangeButton>
+      </style.Section>
+      <style.Section>
+        <style.Label>비밀번호</style.Label>
+        <style.ChangeButton width={140}>비밀번호 변경</style.ChangeButton>
+      </style.Section>
+      <style.ConfirmButton isConfirm>저장하기</style.ConfirmButton>
+    </style.Main>
   </style.Wrapper>
 );
 
