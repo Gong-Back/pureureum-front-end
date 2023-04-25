2571c2c68c70da9dfcf676cbaa1c31b6518e77d0
RookieAND <gwangin1999@naver.com>
2023-04-25 23:47:04 +0900

    PRR-45 [feat]: MediaQuery 관련 스타일 변수 추가

--- src/constants/styles/theme.ts
+++ src/constants/styles/theme.ts
@@ -1,5 +1,6 @@
 import { COLORS } from './color';
 import { FONT_STYLE_PC, FONT_STYLE_MOBILE } from './font';
+import { MEDIA_QUERIES } from './mediaQuery';
 
 export const theme = {
   colors: COLORS,
@@ -7,6 +8,7 @@ export const theme = {
     pc: FONT_STYLE_PC,
     mobile: FONT_STYLE_MOBILE,
   },
+  medias: MEDIA_QUERIES,
 };
 
 export type ThemeType = typeof theme;
