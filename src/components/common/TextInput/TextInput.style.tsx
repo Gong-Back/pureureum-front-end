de5443abc5a40634e0fa5fc9ac473554f8229f9f
ballsona0925 <ballsona0925@naver.com>
2023-04-27 01:23:48 +0900

    PRR-59 [refactor]: TextInput 반응형 적용

--- src/components/common/TextInput/TextInput.style.tsx
+++ src/components/common/TextInput/TextInput.style.tsx
@@ -2,28 +2,35 @@ import styled from '@emotion/styled';
 import { TextInputProps } from './TextInput';
 
 const DEFAULT_SIZES = {
-  small: { width: 300, height: 40 },
-  medium: { width: 400, height: 50 },
-  large: { width: 500, height: 50 },
+  small: { width: 300, height: 45, mHeight: 40 },
+  medium: { width: 400, height: 50, mHeight: 45 },
+  large: { width: 500, height: 50, mHeight: 50 },
 };
 
 export const Input = styled.input<Omit<TextInputProps, 'value'>>(
   ({ theme, isFilled, isRound, sizeType, width }) => {
-    const { colors, fonts } = theme;
+    const { colors, fonts, mediaQueries } = theme;
+    const isSmallSize = sizeType === 'small';
     return {
-      width: width ?? DEFAULT_SIZES[sizeType ?? 'small'].width,
-      height: DEFAULT_SIZES[sizeType ?? 'small'].height,
+      width: width ?? DEFAULT_SIZES[sizeType ?? 'medium'].width,
+      height: DEFAULT_SIZES[sizeType ?? 'medium'].height,
       paddingLeft: 25,
       color: colors.grayscale.gray500,
       border: isFilled ? 'none' : `1px solid ${colors.grayscale.gray500}`,
       borderRadius: isRound ? 30 : 5,
-      backgroundColor: isFilled ? '#F8F8F8' : colors.grayscale.white,
+      backgroundColor: isFilled ? colors.background2 : colors.grayscale.white,
 
-      fontSize: fonts.pc[sizeType === 'small' ? 'body3' : 'body2R'].fontSize,
-      fontWeight:
-        fonts.pc[sizeType === 'small' ? 'body3' : 'body2R'].fontWeight,
-      lineHeight:
-        fonts.pc[sizeType === 'small' ? 'body3' : 'body2R'].lineHeight,
+      fontSize: fonts.pc[isSmallSize ? 'body3' : 'body2R'].fontSize,
+      fontWeight: fonts.pc[isSmallSize ? 'body3' : 'body2R'].fontWeight,
+      lineHeight: fonts.pc[isSmallSize ? 'body3' : 'body2R'].lineHeight,
+
+      [`@media ${mediaQueries.mobile}`]: {
+        height: DEFAULT_SIZES[sizeType ?? 'medium'].mHeight,
+        paddingLeft: 20,
+        fontSize: fonts.mobile[isSmallSize ? 'body3' : 'body2R'].fontSize,
+        fontWeight: fonts.mobile[isSmallSize ? 'body3' : 'body2R'].fontWeight,
+        lineHeight: fonts.mobile[isSmallSize ? 'body3' : 'body2R'].lineHeight,
+      },
     };
   },
 );
