b9e194128145843a373b08f1d13455110897fc94
RookieAND <gwangin1999@naver.com>
2023-05-05 16:10:30 +0900

    PRR-44 [feat]: 규격화된 주소 값을 가져오도록 useDaumPostCode 훅 수정

--- src/hooks/useDaumPostCode.ts
+++ src/hooks/useDaumPostCode.ts
@@ -1,8 +1,17 @@
 import { useEffect, useState } from 'react';
 
+import { AddressType } from '@/constants/types';
+import { COLORS } from '@/constants/styles';
+
 const useDaumPostCode = () => {
   const [postCodeLoaded, setPostCodeLoaded] = useState(false);
-  const [address, setAddress] = useState<string | null>(null);
+  const [address, setAddress] = useState<AddressType>({
+    county: '',
+    city: '',
+    district: '',
+    detail: '',
+    jibun: '',
+  });
 
   // Daum PostCode API Script 로드
   useEffect(() => {
@@ -13,14 +22,47 @@ const useDaumPostCode = () => {
   }, []);
 
   const openPostCode = () => {
-    if (!postCodeLoaded) return;
+    if (typeof window === 'undefined' || !postCodeLoaded) return;
 
-    const { daum } = window;
-    new daum.Postcode({
-      oncomplete(data: { address: string }) {
-        setAddress(data.address);
+    new window.daum.Postcode({
+      // theme: {
+      //   bgColor: COLORS.background2,
+      //   searchBgColor: COLORS.primary.green600,
+      //   pageBgColor: COLORS.grayscale.cremeWhite,
+      //   textColor: COLORS.grayscale.gray700,
+      //   queryTextColor: COLORS.background,
+      //   postcodeTextColor: COLORS.caption,
+      //   emphTextColor: COLORS.primary.green600,
+      //   outlineColor: COLORS.grayscale.gray100,
+      // },
+      oncomplete(data: {
+        sido: string;
+        sigungu: string;
+        bname: string;
+        jibunAddress: string;
+        buildingName: string;
+      }) {
+        const jibun = data.jibunAddress.split(' ').at(-1) ?? '';
+        const {
+          sido: county,
+          sigungu: city,
+          bname: district,
+          buildingName: detail,
+        } = data;
+        setAddress({
+          county,
+          city,
+          district,
+          jibun,
+          detail,
+        });
       },
-    }).open();
+    }).open({
+      left: window.screen.width / 2 - 250,
+      top: window.screen.height / 2 - 300,
+      autoClose: true,
+      popupTitle: '신규 등록 시설 주소 검색',
+    });
   };
 
   return { openPostCode, address };
