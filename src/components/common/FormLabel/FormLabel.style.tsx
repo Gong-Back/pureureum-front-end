b05aff0188e56fbd67632b5ba87f7f2b3e135b11
ballsona <ballsona0925@naver.com>
2023-05-06 15:05:30 +0900

    PRR-53 [feat]: FormLabel 공용 컴포넌트 추가

---
+++ src/components/common/FormLabel/FormLabel.style.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+
+  > .asterisk {
+    margin-left: 10px;
+  }
+`;
