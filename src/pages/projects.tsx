e65c6cbec49e0fd98f21981d2ce668daa5033166
ballsona0925 <ballsona0925@naver.com>
2023-05-11 03:48:27 +0900

    PRR-69 [feat]: projects 페이지 파일 추가

---
+++ src/pages/projects.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next';
+import ProjectListTemlate from '@/components/template/ProjectListTemplate';
+
+const ProjectsPage: NextPage = () => <ProjectListTemlate />;
+
+export default ProjectsPage;
