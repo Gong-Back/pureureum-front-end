9a6907f2e6f0119501e631c396b13ce7f2f2fec4
RookieAND <gwangin1999@naver.com>
2023-05-17 00:19:38 +0900

    PRR-63 [feat]: kakaoMap, daumPostCode Script 로직 추가

--- src/hooks/useDaumPostCode.ts
+++ src/hooks/useDaumPostCode.ts
@@ -4,7 +4,10 @@ import { AddressType } from '@/constants/types';
 import { COLORS } from '@/constants/styles';
 
 const useDaumPostCode = () => {
-  const [postCodeLoaded, setPostCodeLoaded] = useState(false);
+  const [sdkScriptLoaded, setSdkScriptLoaded] = useState({
+    daumPostCode: false,
+    kakaoMap: false,
+  });
   const [address, setAddress] = useState<AddressType>({
     county: '',
     city: '',
@@ -13,29 +16,34 @@ const useDaumPostCode = () => {
     jibun: '',
   });
 
-  // Daum PostCode API Script 로드
+  // TODO : 두 개의 Script 를 useEffect 에서 로드하는 것 말고, next/script로 로드하는 방법 강구해보기.
   useEffect(() => {
-    const $script = document.createElement('script');
-    $script.src = `https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js`;
-    $script.addEventListener('load', () => setPostCodeLoaded(true));
-    document.head.appendChild($script);
+    const postCodeScript = document.createElement('script');
+    postCodeScript.src = `https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js`;
+    postCodeScript.addEventListener('load', () =>
+      setSdkScriptLoaded((prev) => ({ ...prev, daumPostCode: true })),
+    );
+
+    const kakaoMapScript = document.createElement('script');
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`;
+    kakaoMapScript.addEventListener('load', () =>
+      setSdkScriptLoaded((prev) => ({ ...prev, kakaoMap: true })),
+    );
+
+    document.head.appendChild(postCodeScript);
+    document.head.appendChild(kakaoMapScript);
   }, []);
 
-  const openPostCode = () => {
-    if (typeof window === 'undefined' || !postCodeLoaded) return;
+  const isAllScriptLoaded = Object.values(sdkScriptLoaded).every(Boolean);
 
-    new window.daum.Postcode({
-      // theme: {
-      //   bgColor: COLORS.background2,
-      //   searchBgColor: COLORS.primary.green600,
-      //   pageBgColor: COLORS.grayscale.cremeWhite,
-      //   textColor: COLORS.grayscale.gray700,
-      //   queryTextColor: COLORS.background,
-      //   postcodeTextColor: COLORS.caption,
-      //   emphTextColor: COLORS.primary.green600,
-      //   outlineColor: COLORS.grayscale.gray100,
-      // },
+  const openPostCode = () => {
+    if (typeof window === 'undefined' || !isAllScriptLoaded) return;
+
+    const { kakao, daum } = window;
+
+    new daum.Postcode({
       oncomplete(data: {
+        address: string;
         sido: string;
         sigungu: string;
         bname: string;
@@ -62,6 +70,11 @@ const useDaumPostCode = () => {
       top: window.screen.height / 2 - 300,
       autoClose: true,
       popupTitle: '신규 등록 시설 주소 검색',
+    });
+
+    kakao.maps.load(() => {
+      const geocoder = new kakao.maps.services.Geocoder();
+      geocoder.addressSearch();
     });
   };
 
