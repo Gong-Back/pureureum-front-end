a28ef0dec09cae82bb157ff94e2e9578df508c03
Baik Gwangin <gwangin1999@naver.com>
2023-07-16 02:03:02 +0900

    feat: LoginInput에 LoginContext 관련 로직 적용

--- src/components/domain/Login/LoginForm/LoginForm.tsx
+++ src/components/domain/Login/LoginForm/LoginForm.tsx
@@ -1,57 +1,74 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import { useSetAtom } from 'jotai';
 
 import TextInput from '@/components/common/TextInput';
 import Button from '@/components/common/Button';
-import * as style from './LoginForm.style';
+import {
+  useLoginContextValue,
+  useLoginContextAction,
+} from '@/stores/context/LoginContext';
+import { handleTokenAtom } from '@/stores/atom/token/actions';
 
-interface LoginFormProps {
-  /** 아이디로 사용되는 이메일 값 */
-  email: string;
-  /** 패스워드 값 */
-  password: string;
-  /** 입력값을 관리하는 함수 */
-  handleLoginInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  /** 로그인 버튼을 눌렀을 때 실행되는 함수 */
-  submitLogin: () => Promise<void>;
-}
+import * as style from './LoginForm.style';
 
 /**
  * 로그인 관련 정보를 입력받는 Form 컴포넌트
  */
-const LoginForm = ({
-  email,
-  password,
-  handleLoginInput,
-  submitLogin,
-}: LoginFormProps) => (
-  <style.Wrapper>
-    <TextInput
-      value={email}
-      placeholder="E-Mail"
-      name="email"
-      type="email"
-      onChange={handleLoginInput}
-      className="login-input"
-      isRound
-    />
-    <TextInput
-      value={password}
-      placeholder="Password"
-      name="password"
-      type="password"
-      onChange={handleLoginInput}
-      className="login-input"
-      isRound
-    />
-    <Button
-      onClick={submitLogin}
-      className="login-button"
-      sizeType="large"
-      isFilled
-    >
-      로그인
-    </Button>
-  </style.Wrapper>
-);
+const LoginForm = () => {
+  const router = useRouter();
+
+  const { email, password } = useLoginContextValue();
+  const { change, submit, reset } = useLoginContextAction();
+  const handleToken = useSetAtom(handleTokenAtom);
+
+  const handleLoginInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { type, value } = e.target;
+    change(type, value);
+  };
+
+  const submitLogin = async () => {
+    if (!email || !password) return;
+
+    try {
+      const { accessToken, refreshToken } = await submit();
+      handleToken({ action: 'LOGIN', value: { accessToken, refreshToken } });
+      router.replace('/');
+    } catch (err) {
+      reset();
+    }
+  };
+
+  return (
+    <style.Wrapper>
+      <TextInput
+        value={email}
+        placeholder="E-Mail"
+        name="email"
+        type="email"
+        onChange={handleLoginInput}
+        className="login-input"
+        isRound
+      />
+      <TextInput
+        value={password}
+        placeholder="Password"
+        name="password"
+        type="password"
+        onChange={handleLoginInput}
+        className="login-input"
+        isRound
+      />
+      <Button
+        onClick={submitLogin}
+        className="login-button"
+        sizeType="large"
+        isFilled
+      >
+        로그인
+      </Button>
+    </style.Wrapper>
+  );
+};
 
 export default LoginForm;
