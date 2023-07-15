a09bdbfc2b515af1813cbc99ce017f6d58b0fed9
Baik Gwangin <gwangin1999@naver.com>
2023-07-16 02:15:19 +0900

    refactor: 불필요한 컴포넌트 분리를 취소하고 Template에 병합

--- src/components/template/LoginTemplate/LoginTemplate.tsx
+++ src/components/template/LoginTemplate/LoginTemplate.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import { forwardRef } from 'react';
+import Link from 'next/link';
+
 import PureureumPcLogoSvg from '@/assets/icons/pureureumPcLogo.svg';
+import GoogleCircleIconSvg from '@/assets/icons/googleCircleIcon.svg';
+import NaverCircleIconSvg from '@/assets/icons/naverCircleIcon.svg';
+import KakaoCircleIconSvg from '@/assets/icons/kakaoCircleIcon.svg';
 
 import Text from '@/components/common/Text';
+import Button from '@/components/common/Button';
 import LoginForm from '@/components/domain/Login/LoginForm';
-import RegisterSection from '@/components/domain/Login/RegisterSection';
-
 import { COLORS } from '@/constants/styles';
-import * as style from './LoginTemplate.style';
 
+import * as style from './LoginTemplate.style';
 
 const LoginTemplate = () => (
   <style.Wrapper>
@@ -18,7 +22,46 @@ const LoginTemplate = () => (
       </Text>
     </style.Header>
     <LoginForm />
-    <RegisterSection />
+    <style.Register>
+      <style.Section>
+        <Text
+          fontStyleName="body1R"
+          color={COLORS.grayscale.gray500}
+          className="description"
+        >
+          소셜 로그인
+        </Text>
+        <style.SocialIcons>
+          <Link href="/oauth2/google" passHref legacyBehavior>
+            <GoogleCircleIconSvg />
+          </Link>
+          <Link href="/oauth2/kakao" passHref legacyBehavior>
+            <KakaoCircleIconSvg />
+          </Link>
+          <Link href="/oauth2/naver" passHref legacyBehavior>
+            <NaverCircleIconSvg />
+          </Link>
+        </style.SocialIcons>
+      </style.Section>
+      <style.Section>
+        <Text
+          fontStyleName="body1R"
+          color={COLORS.grayscale.gray500}
+          className="description"
+        >
+          계정이 없다면
+        </Text>
+        <Link href="/auth/register">
+          <Button
+            themeColor={COLORS.primary.greenDefault}
+            className="register-button"
+            isRound
+          >
+            회원가입
+          </Button>
+        </Link>
+      </style.Section>
+    </style.Register>
   </style.Wrapper>
 );
 export default LoginTemplate;
