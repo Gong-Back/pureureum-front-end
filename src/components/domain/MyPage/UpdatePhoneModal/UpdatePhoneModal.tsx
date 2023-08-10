3cd1d567dc18d2802206971667650e0cb6939372
RookieAND <gwangin1999@naver.com>
2023-08-10 15:35:12 +0900

    fix: 누락되었던 FormProvider 적용

--- src/components/domain/MyPage/UpdatePhoneModal/UpdatePhoneModal.tsx
+++ src/components/domain/MyPage/UpdatePhoneModal/UpdatePhoneModal.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import { AuthRepository } from '@/apis/auth';
+import { UserRepository } from '@/apis/user';
+
 import {
   useForm,
   FormProvider,
   useWatch,
   SubmitHandler,
 } from 'react-hook-form';
-
-import { AuthRepository } from '@/apis/auth';
-import { UserRepository } from '@/apis/user';
 
 import Button from '@/components/common/Button';
 import Text from '@/components/common/Text';
@@ -21,7 +20,6 @@ import useModal from '@/hooks/useModal';
 import ValidationUtil from '@/utils/validation';
 
 import * as style from './UpdatePhoneModal.style';
-
 
 const UpdatePhoneModal = () => {
   const { closeModal } = useModal();
@@ -104,67 +102,70 @@ const UpdatePhoneModal = () => {
   };
 
   return (
-    <ModalTemplate title="휴대폰 번호 변경하기">
-      <style.Wrapper>
-        <Text
-          color={COLORS.grayscale.gray500}
-          fontStyleName="body3"
-          className="title"
-        >
-          변경할 번호를 입력해주시고 SMS 인증을 진행해주세요
-        </Text>
-        <TextInput
-          placeholder="전화번호"
-          name="changedPhoneNumber"
-          formatValue={(value) =>
-            value
-              .replace(/[^0-9]/g, '')
-              .slice(0, 11)
-              .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)
-          }
-          isRound
-          sizeType="medium"
-          className="phone-input"
-          disabled={isSendingVerifyNum}
-        />
-        <Button
-          isFilled
-          sizeType="medium"
-          className="verify-btn"
-          themeColor={
-            isValidPhoneNumber && !isSendingVerifyNum
-              ? COLORS.primary.greenDefault
-              : COLORS.grayscale.gray400
-          }
-          onClick={sendSmsCertificationNumber}
-        >
-          {!isSendingVerifyNum ? '인증번호 요청' : '요청 전송됨'}
-        </Button>
-        <TextInput
-          placeholder="인증번호"
-          name="confirmedNumber"
-          isRound
-          sizeType="medium"
-          className="verify-input"
-          disabled={!isSendingVerifyNum}
-          formatValue={(value) => value.replace(/[^0-9]/g, '').slice(0, 6)}
-          maxLength={6}
-        />
-        <Button
-          isFilled
-          themeColor={
-            isPossibleToConfirm
-              ? COLORS.primary.greenDefault
-              : COLORS.grayscale.gray400
-          }
-          sizeType="small"
-          className="confirm-btn"
-          onClick={handleSubmit(confirmUpdatePhoneNumber)}
-        >
-          변경 완료
-        </Button>
-      </style.Wrapper>
-    </ModalTemplate>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FormProvider {...formMethods}>
+      <ModalTemplate title="휴대폰 번호 변경하기">
+        <style.Wrapper>
+          <Text
+            color={COLORS.grayscale.gray500}
+            fontStyleName="body3"
+            className="title"
+          >
+            변경할 번호를 입력해주시고 SMS 인증을 진행해주세요
+          </Text>
+          <TextInput
+            placeholder="전화번호"
+            name="changedPhoneNumber"
+            formatValue={(value) =>
+              value
+                .replace(/[^0-9]/g, '')
+                .slice(0, 11)
+                .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)
+            }
+            isRound
+            sizeType="medium"
+            className="phone-input"
+            disabled={isSendingVerifyNum}
+          />
+          <Button
+            isFilled
+            sizeType="medium"
+            className="verify-btn"
+            themeColor={
+              isValidPhoneNumber && !isSendingVerifyNum
+                ? COLORS.primary.greenDefault
+                : COLORS.grayscale.gray400
+            }
+            onClick={sendSmsCertificationNumber}
+          >
+            {!isSendingVerifyNum ? '인증번호 요청' : '요청 전송됨'}
+          </Button>
+          <TextInput
+            placeholder="인증번호"
+            name="confirmedNumber"
+            isRound
+            sizeType="medium"
+            className="verify-input"
+            disabled={!isSendingVerifyNum}
+            formatValue={(value) => value.replace(/[^0-9]/g, '').slice(0, 6)}
+            maxLength={6}
+          />
+          <Button
+            isFilled
+            themeColor={
+              isPossibleToConfirm
+                ? COLORS.primary.greenDefault
+                : COLORS.grayscale.gray400
+            }
+            sizeType="small"
+            className="confirm-btn"
+            onClick={handleSubmit(confirmUpdatePhoneNumber)}
+          >
+            변경 완료
+          </Button>
+        </style.Wrapper>
+      </ModalTemplate>
+    </FormProvider>
   );
 };
 
