706f6fb57803087dc150780515857c4f815c2f90
Sona Gong <ballsona0925@naver.com>
2023-08-10 19:09:33 +0900

    PRR-81 add project register api  (#26)

--- src/components/domain/Project/ProjecItem/ProjectItem.tsx
+++ src/components/domain/Project/ProjecItem/ProjectItem.tsx
@@ -6,29 +6,28 @@ import LocationIconSvg from '@/assets/icons/locationIcon.svg';
 import CalendarIconSvg from '@/assets/icons/calendarIcon.svg';
 import GrassIconSvg from '@/assets/icons/grassIcon.svg';
 
-import { ProjectItemType } from '@/constants/types';
+import { CategoryType, ProjectBasicInfoType } from '@/constants/types';
 import { COLORS } from '@/constants/styles';
 
 import * as style from './ProjectItem.style';
 
-export interface ProjectItemProps extends Omit<ProjectItemType, 'projectId'> {}
+export interface ProjectItemProps {
+  category: CategoryType;
+  thumbnail: string;
+  info: ProjectBasicInfoType;
+}
 
 // TODO 멤버 모집률을 반영한 GrassIcon 상태 변화는 추후에 수정하겠습니다
 // TODO FormatUtils 교체 예정
-const ProjectItem = ({
-  type,
-  thumbnail,
-  title,
-  introduction,
-  onwerName,
-  currentRecruit,
-  totalRecruit,
-  location,
-  startDate,
-  endDate,
-}: ProjectItemProps) => {
+const ProjectItem = ({ category, thumbnail, info }: ProjectItemProps) => {
   const [isHovering, setIsHovering] = useState<boolean>(false);
   const onHover = () => setIsHovering((prev) => !prev);
+
+  const { city, county, district } = info.facilityAddress;
+  const location = `${city} ${county} ${district}`;
+
+  const { projectStartDate: s, projectEndDate: e } = info;
+  const duartion = `${s.replaceAll('-', '.')} ~ ${e.replaceAll('-', '.')}`;
 
   return (
     <style.Wrapper onMouseOver={onHover} onMouseOut={onHover}>
@@ -40,18 +39,23 @@ const ProjectItem = ({
               color={COLORS.grayscale.white}
               className="intro-text"
             >
-              {introduction}
+              이곳은 한줄 소개 공간인데 백엔드에서 스프린트 3주차에 주시겠다고
+              그래가지구 못넣었어요 이건 더미데이터에요
             </Text>
             <Text fontStyleName="body2R" color={COLORS.grayscale.white}>
-              {onwerName}
+              {info.title}
             </Text>
           </style.HoveringContentWrap>
         )}
         <style.TagsWrap>
-          <CategoryTag sizeType="small" type={type} className="category-tag" />
+          <CategoryTag
+            type={category}
+            sizeType="small"
+            className="category-tag"
+          />
           <style.MemberStatusTag>
             <Text fontStyleName="body3" color={COLORS.grayscale.gray700}>
-              {currentRecruit} / {totalRecruit}
+              {info.recruits} / {info.totalRecruits}
             </Text>
             <GrassIconSvg />
           </style.MemberStatusTag>
@@ -60,18 +64,18 @@ const ProjectItem = ({
         <Image src={thumbnail} layout="fill" style={{ borderRadius: 20 }} />
       </style.ThumbnailWrap>
       <Text fontStyleName="subtitle2B" color={COLORS.grayscale.gray700}>
-        {title}
+        {info.title}
       </Text>
       <style.InfoWrap>
         <LocationIconSvg color={COLORS.primary.greenDefault} />
         <Text fontStyleName="body2R" color={COLORS.primary.greenDefault}>
-          {location}
+          {`${location}`}
         </Text>
       </style.InfoWrap>
       <style.InfoWrap>
         <CalendarIconSvg color={COLORS.grayscale.gray500} />
         <Text fontStyleName="body2R" color={COLORS.grayscale.gray500}>
-          {startDate.replaceAll('-', '.')} ~ {endDate.replaceAll('-', '.')}
+          {duartion}
         </Text>
       </style.InfoWrap>
     </style.Wrapper>
--- src/constants/apis/server.ts
+++ src/constants/apis/server.ts
@@ -1,2 +1,1 @@
-export const API_URL =
-  'http://ec2-43-202-5-48.ap-northeast-2.compute.amazonaws.com:9091/api/v1' as const;
+export const API_URL = 'https://api.pureureum.site' as const;
