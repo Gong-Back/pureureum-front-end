6f9c2634eae328b2ea4a01b6d3044acd071b928d
ballsona0925@naver.com <ballsona0925@naver.com>
2023-08-28 23:53:26 +0900

    PRR-127 [fix] add dummyData for floatingMenu story

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -1,5 +1,6 @@
 import { ProjectResponses } from './constants/types';
 
+// 프로젝트 리스트 조회 시
 export const projectItemDummyData: ProjectResponses['main'] = {
   projectPartInformation: {
     id: 1,
@@ -30,3 +31,42 @@ export const projectItemDummyData: ProjectResponses['main'] = {
 export const projectsDummydata = Array(10)
   .fill(0)
   .map((v, i) => ({ ...projectItemDummyData, id: i }));
+
+// 프로젝트 상세 데이터 조회 시
+export const projectContentDummyData: ProjectResponses['detail'] = {
+  projectInformation: {
+    title: '우리 봄에 감자 농장 체험해요!',
+    likeCount: 10,
+    projectStartDate: '2023-03-10',
+    projectEndDate: '2023-03-15',
+    recruits: 10,
+    totalRecruits: 100,
+    facilityAddress: {
+      city: '고양시',
+      county: '대화역',
+      district: '어딘가',
+      jibun: '',
+      detail: '',
+      longitude: '',
+      latitude: '',
+    },
+    introduction: 'introduction',
+    content: 'content',
+    minAge: -1,
+    maxAge: -1,
+    guide: '',
+    notice: '',
+    ownerName: 'owner',
+  },
+  projectCategory: 'FARMING_HEALING',
+  projectStatus: 'string',
+  paymentType: 'ENTRY_FEE',
+  projectPayment: '10000원',
+  projectFiles: [
+    { projectFileType: 'THUMBNAIL', projectFileUrl: '' },
+    {
+      projectFileType: 'COMMON',
+      projectFileUrl: '',
+    },
+  ],
+};
