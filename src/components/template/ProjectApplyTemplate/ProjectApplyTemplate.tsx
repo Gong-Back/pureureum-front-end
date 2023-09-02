6871ea82879fe6f36922f17a7a24ae03b46ea3b4
Gwangin Baik <gwangin1999@naver.com>
2023-09-03 02:41:31 +0900

    PRR-60 Add Project Apply Page UI (#31)
    * feat: 프로젝트 신청 페이지 기초 뼈대 및 Mock UI 구축
    
    * feat: 프로젝트 신청 페이지 1차 스타일 레이아웃 적용
    
    * feat: 프로젝트 등록 페이지 등록 및 레이아웃 분리
    
    * feat: add icon svg files for floating menu
    
    * feat: update project types
    
    * feat: add FloatingMenu component
    
    * feat: Grid 레이아웃 적용 및 페이지 내 요소 배치
    
    * PRR-127 [feat] add ownerName in projectType
    
    * PRR-127 [feat] add format F for duration/location
    
    * PRR-127 [feat] update FloatingMenu props type
    
    * PRR-127 [fix] add dummyData for floatingMenu story
    
    * PRR-127 [refactor] move FloatingMenu into Project
    
    * feat: 반응형 레이아웃 일부 적용 및 margin 조정
    
    * feat: 프로젝트 등록 UI 반응형 2차 적용
    
    * feat: 하단 버튼 관련 반응형 레이아웃 디자인 적용
    
    * fix: 자연스러운 반응형 전환 위해 일부 스타일 코드 수정
    
    ---------
    
    Co-authored-by: ballsona0925@naver.com <ballsona0925@naver.com>

---
+++ src/components/template/ProjectApplyTemplate/ProjectApplyTemplate.tsx
@@ -0,0 +1,195 @@
+import { useRouter } from 'next/router';
+
+import ChevronLeftIconSvg from '@/assets/icons/ChevronLeftIcon.svg';
+import HeartIconSvg from '@/assets/icons/heartIcon.svg';
+import BookmarkIconSvg from '@/assets/icons/bookmarkIcon.svg';
+import ShareURLIconSvg from '@/assets/icons/shareURLIcon.svg';
+
+import Button from '@/components/common/Button';
+import Text from '@/components/common/Text';
+import { COLORS } from '@/constants/styles';
+
+import * as styles from './ProjectApplyTemplate.style';
+
+const menuList = [
+  { label: '좋아요', icon: HeartIconSvg, onClick: () => {} },
+  { label: '관심 등록', icon: BookmarkIconSvg, onClick: () => {} },
+  { label: 'URL 공유', icon: ShareURLIconSvg, onClick: () => {} },
+];
+
+const ProjectApplyTemplate = () => {
+  const router = useRouter();
+
+  return (
+    <styles.Wrapper>
+      <styles.MainSection>
+        <styles.Header>
+          <ChevronLeftIconSvg onClick={() => router.back()} />
+          <Text fontStyleName="title" color={COLORS.grayscale.gray700}>
+            프로젝트 신청
+          </Text>
+        </styles.Header>
+        <styles.InfoSection>
+          <Text fontStyleName="subtitle1" color={COLORS.grayscale.gray600}>
+            참가자 정보
+          </Text>
+          <styles.InfoDetail>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              이름
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              공백
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              닉네임
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              gongback
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              나이
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              25세
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              성별
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              여
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              전화번호
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              010-1234-1234
+            </Text>
+          </styles.InfoDetail>
+        </styles.InfoSection>
+        <styles.BankingSection>
+          <Text fontStyleName="subtitle1" color={COLORS.grayscale.gray600}>
+            참가비용 입금
+          </Text>
+          <Text fontStyleName="body2R" color={COLORS.grayscale.gray500}>
+            푸르름은 아직 결제 서비스를 지원하지 않습니다. 하단에 기재되어 있는
+            계좌로 참가비를 송금해주세요.
+          </Text>
+          <styles.BankingDetail>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              계좌
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              293201-04-201136
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              은행
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              국민은행
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              예금주
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              공소나
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              금액
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.primary.dark}>
+              KRW 10,000
+            </Text>
+            <Text fontStyleName="body1B" color={COLORS.grayscale.dark}>
+              문의
+            </Text>
+            <Text fontStyleName="body1R" color={COLORS.grayscale.gray700}>
+              010-1234-1234
+            </Text>
+          </styles.BankingDetail>
+        </styles.BankingSection>
+        <styles.CheckBoxSection>
+          <input type="checkbox" />
+          <Text fontStyleName="body1R" color={COLORS.grayscale.gray500}>
+            참가비용을 입금했나요?
+          </Text>
+          <input type="checkbox" />
+          <Text fontStyleName="body1R" color={COLORS.grayscale.gray500}>
+            프로젝트 유의사항을 제대로 확인했나요?
+          </Text>
+        </styles.CheckBoxSection>
+        <styles.Aside>
+          <Text fontStyleName="subtitle2B" color={COLORS.green.dark}>
+            우리 함께 감자 농장 체험해요!
+          </Text>
+          <Text fontStyleName="body2R" color={COLORS.grayscale.gray600}>
+            그런데 스프링 프레임워크는 기능도 너무 많고 광범위해서 어디서부터
+            어떻게 시작해야 할지 막막합니다.또 너무 많은 유연성을 제공해서, 어떤
+            기술들을 함께 사용해야 할지 선택하기 어렵습니다. 기능이 점점
+            증가하면서 더 많은 설정들이 필요해지기 시작했습니다.
+          </Text>
+          <styles.FacilityDetail>
+            <div className="option">
+              <Text fontStyleName="body2B" color={COLORS.green.default}>
+                주관
+              </Text>
+              <Text fontStyleName="body2R" color={COLORS.grayscale.gray600}>
+                감자농장 소유주
+              </Text>
+            </div>
+            <div className="option">
+              <Text fontStyleName="body2B" color={COLORS.green.default}>
+                위치
+              </Text>
+              <Text fontStyleName="body2R" color={COLORS.grayscale.gray600}>
+                고양시 송산읍 감자농장
+              </Text>
+            </div>
+            <div className="option">
+              <Text fontStyleName="body2B" color={COLORS.green.default}>
+                기간
+              </Text>
+              <Text fontStyleName="body2R" color={COLORS.grayscale.gray600}>
+                2023.03.01 ~ 2023.04.01
+              </Text>
+            </div>
+          </styles.FacilityDetail>
+          <styles.ShareSection>
+            {menuList.map(({ icon: Icon, label, onClick }) => (
+              <styles.ShareField key={label} onClick={onClick}>
+                <Icon />
+                <Text fontStyleName="body2B" color={COLORS.grayscale.gray600}>
+                  {label}
+                </Text>
+              </styles.ShareField>
+            ))}
+          </styles.ShareSection>
+        </styles.Aside>
+      </styles.MainSection>
+      <styles.ButtonSection>
+        <Button
+          themeColor={COLORS.primary.greenDefault}
+          sizeType="medium"
+          className="button"
+          isFilled
+          isRound
+        >
+          <Text fontStyleName="body2B" color={COLORS.grayscale.white}>
+            신청 완료
+          </Text>
+        </Button>
+        <Button
+          themeColor={COLORS.grayscale.gray200}
+          sizeType="medium"
+          className="button"
+          isRound
+        >
+          <Text fontStyleName="body2B" color={COLORS.grayscale.gray200}>
+            이전으로
+          </Text>
+        </Button>
+      </styles.ButtonSection>
+    </styles.Wrapper>
+  );
+};
+
+export default ProjectApplyTemplate;
