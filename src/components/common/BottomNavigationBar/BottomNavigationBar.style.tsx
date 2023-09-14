9ce6c944eeacf31faf1c95b3fa1736b0d690f918
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-14 23:27:52 +0900

    PRR-129 [feat] add BottomNavBar for Mobile

---
+++ src/components/common/BottomNavigationBar/BottomNavigationBar.style.tsx
@@ -0,0 +1,34 @@
+import { COLORS } from '@/constants/styles';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 60px;
+  padding: 10px 20px;
+  background-color: ${COLORS.grayscale.white};
+  box-shadow: 0px 0px 4px 0px rgba(88, 88, 88, 0.25);
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+`;
+
+export const NavItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1px;
+  cursor: pointer;
+  color: ${(props) => props.color};
+
+  & > svg {
+    width: 18px;
+    height: 18px;
+  }
+`;
