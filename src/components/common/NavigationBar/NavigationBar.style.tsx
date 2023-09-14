7ee2d7b0321f256449b7d3ce3ad6998638d79f78
ballsona0925@naver.com <ballsona0925@naver.com>
2023-09-14 23:28:29 +0900

    PRR-129 [feat] modify NavBar style and logo file

--- src/components/common/NavigationBar/NavigationBar.style.tsx
+++ src/components/common/NavigationBar/NavigationBar.style.tsx
@@ -1,32 +1,33 @@
+import { COLORS } from '@/constants/styles';
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.nav`
   display: flex;
   justify-content: space-between;
-
+  align-items: center;
   width: 100%;
   height: 60px;
-
   padding: 0px 30px;
-
+  background-color: ${COLORS.grayscale.white};
   box-shadow: 0px 0px 4px rgba(88, 88, 88, 0.25);
 
-  & > svg {
-    margin: 5px 0 16px 0;
-  }
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
 `;
 
 export const NavItemList = styled.div`
   display: flex;
+  align-items: center;
   gap: 0px 30px;
 
-  margin: auto 0px;
-`;
+  height: 40px;
 
-export const NavItem = styled.span(({ theme }) => {
-  const { colors, fonts } = theme;
-  return {
-    color: colors.grayscale.gray500,
-    ...fonts.pc.body2R,
-  };
-});
+  a {
+    font-size: 17px;
+    line-height: 40px;
+    font-weight: 400;
+    color: ${COLORS.grayscale.gray600};
+  }
+`;
