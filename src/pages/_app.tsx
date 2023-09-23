82b75a89cc348acf75b68eebc8c2af8171601b5b
Baik Gwangin <gwangin1999@naver.com>
2023-09-23 21:51:01 +0900

    feat: 로그인 여부를 전역으로 관리하는 로직 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,26 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, AppContext } from 'next/app';
 import { useState } from 'react';
-
 import { Provider } from 'jotai';
-
-import '@/assets/fonts/font.css';
-import BottomNavigationBar from '@/components/common/BottomNavigationBar';
-import ModalPortal from '@/components/common/ModalPortal';
-import NavigationBar from '@/components/common/NavigationBar';
-import { GlobalStyle, theme } from '@/constants/styles';
-import useMeasureBreakpoint from '@/hooks/useMeasureBreakpoint';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import '@/assets/fonts/font.css';
+
+import BottomNavigationBar from '@/components/common/BottomNavigationBar';
+import ModalPortal from '@/components/common/ModalPortal';
+import NavigationBar from '@/components/common/NavigationBar';
+
+import { GlobalStyle, theme } from '@/constants/styles';
+import useMeasureBreakpoint from '@/hooks/useMeasureBreakpoint';
+
 interface ServiceAppProps {
   isNavigationVisible: boolean;
+  isLogin: boolean;
 }
 
 const MyApp = ({ Component, pageProps }: AppProps<ServiceAppProps>) => {
-  const { isNavigationVisible = true } = pageProps;
-
+  const { isNavigationVisible = true, isLogin = false } = pageProps;
   const currentBreakpoint = useMeasureBreakpoint(['mobile', 'pc']);
   const isMobile = currentBreakpoint === 'mobile';
 
@@ -42,7 +43,7 @@ const MyApp = ({ Component, pageProps }: AppProps<ServiceAppProps>) => {
         <ThemeProvider theme={theme}>
           <Global styles={GlobalStyle} />
           <ModalPortal />
-          {isNavigationVisible && <NavigationBar />}
+          {isNavigationVisible && <NavigationBar isLogin={isLogin} />}
           {/* eslint-disable react/jsx-props-no-spreading */}
           <Component {...pageProps} />
           {isMobile && isNavigationVisible && <BottomNavigationBar />}
@@ -51,5 +52,17 @@ const MyApp = ({ Component, pageProps }: AppProps<ServiceAppProps>) => {
     </QueryClientProvider>
   );
 };
+
+// NOTE : 전역으로 로그인 여부를 판별하기 위해 getInitialProps 사용
+MyApp.getInitialProps = async ({Component, ctx}: AppContext) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  const isLogin = ctx.req?.headers.cookie?.includes('accessToken');
+  pageProps = { ... pageProps, isLogin }
+  
+  return { pageProps };
+}
 
 export default MyApp;
