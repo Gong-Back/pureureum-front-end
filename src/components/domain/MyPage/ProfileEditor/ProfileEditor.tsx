d20a5d6f73730c2b9173b606702245a26aa6a705
Baik Gwangin <gwangin1999@naver.com>
2023-09-24 21:32:13 +0900

    feat: 닉네임 수정 관련 UI 및 API 연결

--- src/components/domain/MyPage/ProfileEditor/ProfileEditor.tsx
+++ src/components/domain/MyPage/ProfileEditor/ProfileEditor.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
-import { useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
 
 import { UserRepository } from '@/apis/user';
 import Button from '@/components/common/Button';
 import Text from '@/components/common/Text';
+import TextInput from '@/components/common/TextInput';
 import QUERY_KEY from '@/constants/apis/queryKey';
 import { COLORS } from '@/constants/styles';
 import useApiMutation from '@/hooks/useApiMutation';
 import useUploadFile from '@/hooks/useUploadFile';
+import { useQueryClient } from '@tanstack/react-query';
 
 import * as styles from './ProfileEditor.style';
 
@@ -17,17 +19,36 @@ interface ProfileEditorProps {
 }
 
 const ProfileEditor = ({ profileUrl, nickname }: ProfileEditorProps) => {
-
   const queryClient = useQueryClient();
-  const { mutateAsync: profileMutate } = useApiMutation<void, File | undefined>({
-    mutationFn: (uploadedFile) => UserRepository.updateProfileImageAsync(uploadedFile),
+  const { mutateAsync: profileMutate } = useApiMutation<void, File | undefined>(
+    {
+      mutationFn: (uploadedFile) =>
+        UserRepository.updateProfileImageAsync(uploadedFile),
+      mutationKey: QUERY_KEY.USER.base,
+      options: {
+        onSuccess: () => {
+          queryClient.invalidateQueries(QUERY_KEY.USER.base);
+        },
+      },
+    },
+  );
+
+  const { mutateAsync: nicknameMutate } = useApiMutation<void, string>({
+    mutationFn: (changedNickname) =>
+      UserRepository.updateUserInfoAsync({
+        type: 'nickname',
+        updatedValue: changedNickname,
+      }),
     mutationKey: QUERY_KEY.USER.base,
     options: {
       onSuccess: () => {
         queryClient.invalidateQueries(QUERY_KEY.USER.base);
-      }
-    }
+      },
+    },
   });
+
+  const [isNicknameInputVisible, setIsNicknameInputVisible] = useState(false);
+  const [changedNickname, setChangedNickname] = useState('');
 
   const { fileInputRef, handleUploadFile } = useUploadFile({
     maxFileSize: 10 * 1024 * 1024,
@@ -37,33 +58,43 @@ const ProfileEditor = ({ profileUrl, nickname }: ProfileEditorProps) => {
 
   const openFileUploadDialog = () => fileInputRef.current?.click();
 
-  const handleChangeNickname = () => {
-    console.log('test');
+  const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setChangedNickname(value);
+  };
+
+  const handleChangeNicknameButton = () => {
+    if (!isNicknameInputVisible) return setIsNicknameInputVisible(true);
+    return changedNickname.length
+      ? nicknameMutate(changedNickname)
+      : setIsNicknameInputVisible(false);
   };
 
   return (
     <styles.Wrapper>
-      {profileUrl ? (
-        <Image
-          src={profileUrl}
-          alt="profileImg"
-          width={120}
-          height={120}
-        />
-      ) : (
-        <styles.DefaultProfileImg />
-      )}
+      <styles.DefaultProfileImg />
       <styles.Section>
-        <Text
-          fontStyleName="subtitle1"
-          color={COLORS.grayscale.gray700}
-          className="nickname"
-        >
-          {nickname || 'test_userId'}
-        </Text>
+        {isNicknameInputVisible ? (
+          <TextInput
+            isFilled
+            sizeType="small"
+            name="changedNickname"
+            onChange={handleChangeNickname}
+            max={20}
+            placeholder="변경할 닉네임을 입력해주세요"
+          />
+        ) : (
+          <Text
+            fontStyleName="subtitle1"
+            color={COLORS.grayscale.gray700}
+            className="nickname"
+          >
+            {nickname || 'test_userId'}
+          </Text>
+        )}
         <styles.ButtonBox>
           <Button
-            onClick={handleChangeNickname}
+            onClick={handleChangeNicknameButton}
             isRound
             sizeType="small"
             themeColor={COLORS.primary.default}
@@ -77,15 +108,26 @@ const ProfileEditor = ({ profileUrl, nickname }: ProfileEditorProps) => {
             style={{ display: 'none' }}
             onChange={handleUploadFile}
           />
-          <Button
-            onClick={openFileUploadDialog}
-            themeColor={COLORS.primary.default}
-            isRound
-            sizeType="small"
-            className="profile-img"
-          >
-            프로필 이미지 변경
-          </Button>
+          {isNicknameInputVisible ? (
+            <Button
+              onClick={() => setIsNicknameInputVisible(false)}
+              isRound
+              sizeType="small"
+              themeColor={COLORS.primary.default}
+            >
+              변경 취소
+            </Button>
+          ) : (
+            <Button
+              onClick={openFileUploadDialog}
+              themeColor={COLORS.primary.default}
+              isRound
+              sizeType="small"
+              className="profile-img"
+            >
+              프로필 이미지 변경
+            </Button>
+          )}
         </styles.ButtonBox>
       </styles.Section>
     </styles.Wrapper>
