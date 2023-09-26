14a55fa1f63e9a38277bfb48cc73ef1d4869775e
Sona Gong <ballsona0925@naver.com>
2023-09-26 13:24:50 +0900

    PRR-156 Add dashboard home layout  (#39)
    * PRR-156 [feat] add dashboard navigation constants
    
    * PRR-156 [feat] add Dashboard layout component
    
    * PRR-156 [feat] add Widget and Template component

---
+++ src/pages/dashboard/[pid].tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next';
+import DashBoardHomeTemplate from '@/components/template/DashBoardHomeTemplate';
+
+const DashBoardHomePage: NextPage = () => <DashBoardHomeTemplate />;
+
+export default DashBoardHomePage;
