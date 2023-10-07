865b185d28bbd2dc912f5255b468cbb2a12c04ea
RookieAND <gwangin1999@naver.com>
2023-10-07 18:48:41 +0900

    feat: 프로젝트 메인 페이지 데이터 관련 Query Hook 추가

---
+++ src/query-hooks/project/query.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { type MainProjectListOutput, ProjectRepository } from '@/apis/project';
+import QUERY_KEY from '@/constants/apis/queryKey';
+import type {
+  ApiError,
+  ApiResponse,
+  ProjectReqParams,
+  ProjectResponses,
+} from '@/constants/types';
+
+export const useGetProjectList = ({
+  searchType,
+  category,
+}: ProjectReqParams['main']) =>
+  useQuery<
+    ApiResponse<MainProjectListOutput>,
+    ApiError,
+    MainProjectListOutput
+  >({
+    queryFn: () =>
+      ProjectRepository.getMainProjectListAsync({ searchType, category }),
+    queryKey: QUERY_KEY.PROJECT.main({ searchType, category }),
+    staleTime: 1000 * 60 * 5,
+  });
