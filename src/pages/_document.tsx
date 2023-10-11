cf1544a0cf4ce8ccbd2b7453ad4b0be954440fd4
Gwangin Baik <gwangin1999@naver.com>
2023-10-11 23:46:44 +0900

    PRR-151 Apply preload logic to local SUIT font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,50 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <link
+        rel="preload"
+        href="/fonts/SUIT-ExtraBold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SUIT-Bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SUIT-SemiBold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SUIT-Medium.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SUIT-Regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/SUIT-Light.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+    </Head>
     <body>
       <div id="modal" />
       <Main />
