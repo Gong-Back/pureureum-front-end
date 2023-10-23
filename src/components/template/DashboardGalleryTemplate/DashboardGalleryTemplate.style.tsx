5634662234d8745ddc35255f7e8b44268ac55b90
ballsona0925@naver.com <ballsona0925@naver.com>
2023-10-23 14:40:37 +0900

    feat: add template components for dashboard

--- src/components/template/DashboardGalleryTemplate/DashboardGalleryTemplate.style.tsx
+++ src/components/template/DashboardGalleryTemplate/DashboardGalleryTemplate.style.tsx
@@ -2,56 +2,14 @@ import styled from '@emotion/styled';
 
 import { MEDIA_QUERIES } from '@/constants/styles';
 
-export const ContentWrapper = styled.div`
+export const ListWrapper = styled.div`
   width: 100%;
+  padding: 70px 50px;
   display: flex;
   flex-direction: column;
   gap: 30px;
-  padding: 30px;
 
   @media ${MEDIA_QUERIES.mobile} {
-    flex-direction: column;
-    padding-bottom: 80px;
-  }
-`;
-
-export const TopContent = styled.div`
-  width: 100%;
-
-  display: flex;
-  align-items: flex-start;
-  gap: 30px;
-
-  .schedule-widget {
-    flex: 1;
-    max-width: 400px;
-    min-height: 400px;
-  }
-  .board-widget {
-    flex: 2;
-    min-height: 400px;
-  }
-
-  .members-widget {
-    flex: 1;
-    max-width: 400px;
-    min-height: 400px;
-  }
-
-  @media ${MEDIA_QUERIES.mobile} {
-    flex-direction: column;
-
-    .schedule-widget,
-    .members-widget {
-      max-width: 100%;
-    }
-  }
-`;
-export const RightContent = styled.div`
-  height: 100%;
-
-  .gallery-widget {
-    height: 100%;
-    min-height: 450px;
+    padding: 50px 30px;
   }
 `;
